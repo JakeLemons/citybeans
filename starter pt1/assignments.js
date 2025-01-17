2e410fb135c49c36f22568e11fae5a8535fb7d6a
JakeLemons <jlemire98@gmail.com>
2025-01-17 00:21:33 -0500

    Add files via upload

--- starter pt1/assignments.js
+++ starter pt1/assignments.js
@@ -7,7 +7,7 @@ console.log(continent);
 console.log(population);
 
 const isIsland = false;
-let language = 'English';
+let language = 'english';
 
 console.log(typeof isIsland);
 console.log(typeof population);
@@ -20,3 +20,59 @@ console.log(population);
 let finlandPopulation = 6000000;
 console.log(population > finlandPopulation);
 let description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+
+
+if (population > 33000000) {
+    console.log(`${country}'s population is above average`);
+} else {
+    console.log(
+        `${country}'s population is ${33000000 - population} below average`
+    );
+}
+
+// console.log('9' - '5'); // 4
+// console.log('19' - '13' + '17'); // 617
+// console.log('19' - '13' + 17); // 23
+// console.log('123' < 57); // false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+
+// const numNeighbours = prompt('How many neighbour countries does your country have?');
+// const numNeighbours = (2);
+// if (numNeighbours === 1) {
+//     console.log('only 1 border');
+// } else if (numNeighbours > 1) {
+//     console.log('more than 1 border');
+// } else {
+//     console.log('No borders');
+// }
+
+
+if (language === 'English' && population < 50000000 && !isIland) {
+    console.log(`You should live in ${country} :)`);
+} else {
+    console.log(`${country} does not meet your criteria:(`);
+};
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
+
+console.log(`${country}'s population is ${population > 33000000 ? 'above' : 'below'} average`);
+console.log(`${country}'s population is ${population}`)
--- starter pt1/script.js
+++ starter pt1/script.js
@@ -1,26 +1,103 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
-console.log(23);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Jim';
+// let firstName = 'Jim';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonasAndMatilda = 'JM';
-let $function = 27;
+// let jonasAndMatilda = 'JM';
+// let $function = 27;
 
-let Person = 'Jim';
-let PI = 3.1415;
+// let Person = 'Jim';
+// let PI = 3.1415;
 
-let myFirstJob = 'coder';
-let myCurrentJob = 'Teacher';
+// let myFirstJob = 'coder';
+// let myCurrentJob = 'Teacher';
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+// let job1 = 'programmer';
+// let job2 = 'teacher';
 
-console.log(myFirstJob);
-console.log(myCurrentJob);
+// console.log(myFirstJob);
+// console.log(myCurrentJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "jim");
+
+// javascriptIsFun = 'Yes!';
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age: number
+// let age = 26;
+// age = 26;
+
+// const birthYear = 1998;
+// // birthYear = 1990;
+// //const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'smith';
+// console.log(lastName);
+
+// const now = 2077;
+// const ageJake = now - 1998;
+// const ageJim = now - 2018;
+// console.log(ageJake, ageJim);
+
+// console.log(ageJake * 2, ageJake / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jake';
+// const lastName = 'Smith';
+// console.log(firstName + lastName);
+// console.log(firstName + ' ' + lastName);
+
+// let x = 10 + 5; // 15
+// x += 10; //x = x + 10 = 25
+// console.log(x);
+// x *= 4; // x = x * 4 = 100
+// console.log(x);
+// x++; // x = x + 1 = 101
+// console.log(x);
+// x--; // x = x - 1 = 99
+// console.log(x);
+
+//comparison operators
+// console.log(ageJake > ageJim); // >, <, >=, <=
+// console.log(ageJim >= 18);
+
+// const isFullAge = ageJim >= 18;
+
+// console.log(now - 1998 > now - 2018);
+
+const now = 2077;
+const ageJake = now - 1998;
+const ageJim = now - 2018;
+
+console.log(now - 1998 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJake + ageJim) / 2;
+console.log(ageJake, ageJim, averageAge);

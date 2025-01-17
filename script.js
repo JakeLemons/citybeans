f1e76d439712bafa626106ef23539abf3006008e
JakeLemons <jlemire98@gmail.com>
2025-01-16 19:43:17 -0500

    My Course Master Files
    These are files from https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22846813?start=1#overview
    
    This is for my personal use, to keep track of my current code and progress across my devices. 
    
    These show my work along with the provided files from the course. 
    
    (I will update this as I learn to format my github better.)

---
+++ script.js
@@ -0,0 +1,26 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jim';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonasAndMatilda = 'JM';
+let $function = 27;
+
+let Person = 'Jim';
+let PI = 3.1415;
+
+let myFirstJob = 'coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
